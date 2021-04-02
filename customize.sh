bff11814dc10ec3b870034befded4b95e7db2c55
Danny Lin <danny@kdrag0n.dev>
2021-04-02 15:28:15 -0700

    customize: Add Android version check
    Users keep trying to use this module on Android 11.

--- customize.sh
+++ customize.sh
@@ -1,3 +1,9 @@
 #!/sbin/sh
 
+sdk="$(getprop ro.build.version.sdk)"
+if ! getprop ro.build.fingerprint | grep -q SPP; then
+    ui_print "This module is for Android 12 previews ONLY."
+    exit 1
+fi
+
 chmod 755 $MODPATH/service.sh $MODPATH/service.payload.sh
