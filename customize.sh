a89153ff99ef73a02f2dc6bf0eba9f50f673d887
Danny Lin <danny@kdrag0n.dev>
2021-04-22 21:30:27 -0700

    customize: Add explicit check for DP3 or newer versions
    We've dropped support for DP2 and older.

--- customize.sh
+++ customize.sh
@@ -1,8 +1,8 @@
-#!/sbin/sh
+#!/system/bin/sh
 
-sdk="$(getprop ro.build.version.sdk)"
-if ! getprop ro.build.fingerprint | grep -q SPP; then
-    ui_print "This module is for Android 12 previews ONLY."
+dp_ver="$(getprop ro.build.id | cut -d. -f1 | sed 's/SPP//')"
+if ! getprop ro.build.id | grep -q SPP || [[ "$dp_ver" -lt 3 ]]; then
+    ui_print "This module is ONLY for Android 12 Developer Preview 3."
     exit 1
 fi
 
