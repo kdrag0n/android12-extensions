f42a1278af2f6eff041f9c4a50f7711b471b39ec
Danny Lin <danny@kdrag0n.dev>
2021-04-22 20:51:07 -0700

    overlays: Remove all overlays
    There's no reason to keep these now that we've decided to drop support
    for older DP versions.

--- overlays/framework/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "$0")"
-
-apktool b .
-
-$ANDROID_HOME/build-tools/31.0.0-rc1/zipalign -f 4 dist/FrameworkFlags.apk{,.new}
-mv dist/FrameworkFlags.apk{.new,}
-$ANDROID_HOME/build-tools/31.0.0-rc1/apksigner sign --ks-pass pass:abcdef --key-pass pass:abcdef --ks ../monet/out/test.jks dist/FrameworkFlags.apk
