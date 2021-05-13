1683b034c2bf6dcf56682ccab7a6594bf54a77da
Danny Lin <danny@kdrag0n.dev>
2021-05-13 16:09:01 -0700

    ui: mods: Add debug option to generate reference color tables
    This is used to get reference colors for Monet.

--- app/src/main/java/dev/kdrag0n/android12ext/ui/settings/mods/ModsSettingsViewModel.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/ui/settings/mods/ModsSettingsViewModel.kt
@@ -3,9 +3,13 @@ package dev.kdrag0n.android12ext.ui.settings.mods
 import android.app.Application
 import de.Maxr1998.modernpreferences.PreferenceScreen
 import de.Maxr1998.modernpreferences.PreferencesAdapter
+import de.Maxr1998.modernpreferences.helpers.categoryHeader
+import de.Maxr1998.modernpreferences.helpers.onClick
 import de.Maxr1998.modernpreferences.helpers.pref
+import dev.kdrag0n.android12ext.BuildConfig
 import dev.kdrag0n.android12ext.R
 import dev.kdrag0n.android12ext.core.data.SettingsRepository
+import dev.kdrag0n.android12ext.core.monet.ReferenceGenerator
 import dev.kdrag0n.android12ext.ui.settings.BaseSettingsViewModel
 import dev.kdrag0n.android12ext.ui.utils.buildWithPrefs
 import dev.kdrag0n.android12ext.ui.utils.featureSwitch
@@ -14,6 +18,7 @@ import dev.kdrag0n.android12ext.ui.utils.setInteractive
 class ModsSettingsViewModel(
     app: Application,
     private val settingsRepo: SettingsRepository,
+    private val refGen: ReferenceGenerator,
 ) : BaseSettingsViewModel(app) {
     private val prefScreen = PreferenceScreen.Builder(app).run {
         featureSwitch(
@@ -28,6 +33,20 @@ class ModsSettingsViewModel(
             summaryRes = R.string.mods_info
             // Disabling the view makes the text contrast too low, so use our extension instead
             setInteractive(false)
+        }
+
+        // Debug
+        if (BuildConfig.DEBUG) {
+            categoryHeader("debug_header") {
+                title = "Debug"
+            }
+            pref("gen_ref") {
+                title = "Generate reference color table"
+                onClick {
+                    refGen.gen()
+                    false
+                }
+            }
         }
 
         buildWithPrefs(settingsRepo.prefs)
