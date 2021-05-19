52a14a6407b4aad63d35dc8b33c8abc7bcf8ca16
Danny Lin <danny@kdrag0n.dev>
2021-05-19 01:47:43 -0700

    monet: theme: Generate accent3 with a 60-degree hue offset
    Also document what I've learned about the colors while we're at it.

--- app/src/main/java/dev/kdrag0n/android12ext/core/monet/theme/DynamicColorScheme.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/core/monet/theme/DynamicColorScheme.kt
@@ -1,7 +1,6 @@
 package dev.kdrag0n.android12ext.core.monet.theme
 
 import dev.kdrag0n.android12ext.core.monet.colors.Color
-import dev.kdrag0n.android12ext.core.monet.colors.LinearSrgb.Companion.toLinearSrgb
 import dev.kdrag0n.android12ext.core.monet.colors.Oklab.Companion.toOklab
 import dev.kdrag0n.android12ext.core.monet.colors.Oklch
 import dev.kdrag0n.android12ext.core.monet.colors.Oklch.Companion.toOklch
@@ -14,18 +13,28 @@ class DynamicColorScheme(
 ) : ColorScheme() {
     private val primaryLch = Srgb(primaryColor).toLinearSrgb().toOklab().toOklch()
 
+    // Main background color. Tinted with the primary color.
     override val neutral1 = transformQuantizedColors(targetColors.neutral1)
+    // Secondary background color. Slightly tinted with the primary color.
     override val neutral2 = transformQuantizedColors(targetColors.neutral2)
 
+    // Main accent color. Generally, this is close to the primary color.
     override val accent1 = transformQuantizedColors(targetColors.accent1)
+    // Secondary accent color. Darker shades of accent1.
     override val accent2 = transformQuantizedColors(targetColors.accent2)
-    override val accent3 = transformQuantizedColors(targetColors.accent3)
+    // Tertiary accent color. Primary color shifted to the next secondary color via hue offset.
+    override val accent3 = transformQuantizedColors(targetColors.accent3) { lch ->
+        lch.copy(h = lch.h + ACCENT3_HUE_OFFSET_DEGREES)
+    }
 
-    private fun transformQuantizedColors(colors: List<Color>): List<Color> {
+    private fun transformQuantizedColors(
+        colors: List<Color>,
+        colorFilter: (Oklch) -> Oklch = { it }
+    ): List<Color> {
         return colors.withIndex().map { colorEntry ->
             val colorLch = colorEntry.value as? Oklch
                     ?: colorEntry.value.toLinearSrgb().toOklab().toOklch()
-            val newLch = transformColor(colorLch)
+            val newLch = colorFilter(transformColor(colorLch))
             val newColor = newLch.toOklab().toLinearSrgb().toSrgb()
 
             val newRgb8 = newColor.quantize8()
@@ -43,5 +52,11 @@ class DynamicColorScheme(
             // Use the primary color's hue, since it's the most prominent feature of the theme.
             h = primaryLch.h,
         )
+    }
+
+    companion object {
+        // Hue offset for the tertiary accent color (accent3), in degrees.
+        // 60 degrees = shifting by a secondary color
+        private const val ACCENT3_HUE_OFFSET_DEGREES = 60.0
     }
 }
