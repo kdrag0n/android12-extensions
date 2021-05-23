536062d83e332006a0391187ac1f51f5711aff99
Danny Lin <danny@kdrag0n.dev>
2021-05-22 19:38:38 -0700

    monet: colors: Add common interface for LCh representations

--- app/src/main/java/dev/kdrag0n/android12ext/monet/colors/Oklch.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/monet/colors/Oklch.kt
@@ -1,34 +1,24 @@
 package dev.kdrag0n.android12ext.monet.colors
 
-import kotlin.math.*
+import dev.kdrag0n.android12ext.monet.colors.Lch.Companion.toLab
+import dev.kdrag0n.android12ext.monet.colors.Lch.Companion.toLch
 
 data class Oklch(
-    val L: Double,
-    val C: Double,
-    val h: Double,
-) : Color {
+    override val L: Double,
+    override val C: Double,
+    override val h: Double,
+) : Color, Lch {
     override fun toLinearSrgb() = toOklab().toLinearSrgb()
 
     fun toOklab(): Oklab {
-        val hRad = Math.toRadians(h)
-
-        return Oklab(
-            L = L,
-            a = C * cos(hRad),
-            b = C * sin(hRad),
-        )
+        val (l, a, b) = toLab()
+        return Oklab(l, a, b)
     }
 
     companion object {
         fun Oklab.toOklch(): Oklch {
-            val hDeg = Math.toDegrees(atan2(b, a))
-
-            return Oklch(
-                L = L,
-                C = sqrt(a.pow(2) + b.pow(2)),
-                // Normalize the angle, as many will be negative
-                h = if (hDeg < 0) hDeg + 360 else hDeg,
-            )
+            val (l, c, h) = toLch()
+            return Oklch(l, c, h)
         }
     }
 }
