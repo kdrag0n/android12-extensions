9ef165d74e6edbf0ca8bdc32e3619540d6a3a24d
Danny Lin <danny@kdrag0n.dev>
2021-06-13 16:16:45 -0700

    hooks: launcher: Update flags for Beta 2

--- app/src/main/java/dev/kdrag0n/android12ext/core/xposed/hooks/LauncherHooks.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/core/xposed/hooks/LauncherHooks.kt
@@ -9,33 +9,8 @@ class LauncherHooks(
     private val lpparam: XC_LoadPackage.LoadPackageParam,
 ) {
     val flagValues = mapOf(
-        //"ENABLE_DATABASE_RESTORE" to true,
-        //"ENABLE_SMARTSPACE_UNIVERSAL" to true,
-        "ENABLE_SMARTSPACE_ENHANCED" to true,
-        //"ALWAYS_USE_HARDWARE_OPTIMIZATION_FOR_FOLDER_ANIMATIONS" to true,
-        //"SEPARATE_RECENTS_ACTIVITY" to true,
-        //"ENABLE_MINIMAL_DEVICE" to true,
-        "EXPANDED_SMARTSPACE" to true,
-        "ENABLE_FOUR_COLUMNS" to true,
-        "ENABLE_LAUNCHER_ACTIVITY_THEME_CROSSFADE" to true,
-        "ENABLE_SPLIT_SELECT" to true,
-        "PROMISE_APPS_IN_ALL_APPS" to true,
-        //"UNSTABLE_SPRINGS" to true,
-        "KEYGUARD_ANIMATION" to true,
-        "ENABLE_QUICKSTEP_LIVE_TILE" to true,
         "ENABLE_DEVICE_SEARCH" to true,
-        //"FORCE_LOCAL_OVERSCROLL_PLUGIN" to true,
-        //"ASSISTANT_GIVES_LAUNCHER_FOCUS" to true,
-        //"HOTSEAT_MIGRATE_TO_FOLDER" to true,
-        "ENABLE_WIDGETS_PICKER_AIAI_SEARCH" to true,
-        //"ENABLE_OVERVIEW_SHARE" to true,
-        "ENABLE_OVERVIEW_SHARING_TO_PEOPLE" to true,
-        "ENABLE_OVERVIEW_CONTENT_PUSH" to true,
-
-        // Tablets only
-        "ENABLE_TASKBAR" to true,
-        "ENABLE_OVERVIEW_GRID" to true,
-        "ENABLE_TWO_PANEL_HOME" to true,
+        "ENABLE_THEMED_ICONS" to true,
     )
 
     fun applyFeatureFlags() {
@@ -43,7 +18,7 @@ class LauncherHooks(
             override fun afterHookedMethod(param: MethodHookParam) {
                 val key = param.args[0] as String
                 Timber.i("Hooking launcher flag: $key")
-                XposedHelpers.setBooleanField(param.thisObject, CURRENT_VALUE_FIELD, flagValues[key] ?: return)
+                XposedHelpers.setBooleanField(param.thisObject, VALUE_FIELD, flagValues[key] ?: return)
             }
         }
 
@@ -57,10 +32,7 @@ class LauncherHooks(
     }
 
     companion object {
-        // com.android.launcher3.config.FeatureFlags$BooleanFlag
-        private const val BOOLEAN_FLAG_CLASS = "T.a"
-
-        // BooleanFlag.mCurrentValue
-        private const val CURRENT_VALUE_FIELD = "c"
+        private const val BOOLEAN_FLAG_CLASS = "com.android.launcher3.config.FeatureFlags\$BooleanFlag"
+        private const val VALUE_FIELD = "defaultValue"
     }
 }
