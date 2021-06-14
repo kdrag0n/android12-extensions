651ff7d1ad17e5daf7366bb20c611b89e4780ec2
Danny Lin <danny@kdrag0n.dev>
2021-06-14 13:54:09 -0700

    monet: theme: Add target lightness map in CIELAB L*
    Google specifies the lightness of Material You color targets in CIELAB
    lightness (L*), as can be seen in the AOSP documentation. Add the
    official lightness map to better match the targets.

--- app/src/main/java/dev/kdrag0n/android12ext/monet/theme/TargetColors.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/monet/theme/TargetColors.kt
@@ -31,6 +31,23 @@ class TargetColors(
             1000 to 0.000,
         )
 
+        // Lightness map in CIELAB L*
+        val LSTAR_LIGHTNESS_MAP = mapOf(
+            0    to 100.0,
+            10   to  99.0,
+            50   to  95.0,
+            100  to  90.0,
+            200  to  80.0,
+            300  to  70.0,
+            400  to  60.0,
+            500  to  49.6,
+            600  to  40.0,
+            700  to  30.0,
+            800  to  20.0,
+            900  to  10.0,
+            1000 to   0.0,
+        )
+
         // Neutral chroma from Google's CAM16 implementation
         private const val NEUTRAL1_CHROMA = 0.0132
         private const val NEUTRAL2_CHROMA = NEUTRAL1_CHROMA / 2
