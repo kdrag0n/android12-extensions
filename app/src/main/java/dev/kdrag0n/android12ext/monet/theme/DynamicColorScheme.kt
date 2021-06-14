055dcabc4899eb60a5fa3101ed26586584154bc9
Danny Lin <danny@kdrag0n.dev>
2021-06-14 13:58:46 -0700

    monet: theme: Search for L to target CIELAB L* reference
    Ideally, uniform color spaces would have perfectly uniform lightness as
    chroma and hue changes, but unfortunately that's not the case. In
    particular, colors at the extreme ends of lightness (white and black;
    especially shade ~10) diverge from targets significantly.
    
    Searching for the optimal Oklab L value to match the Material You
    targets, which specify lightness in terms of CIELAB L*, nearly
    eliminates the problem. A binary search is used for speed and accuracy,
    similar to what Google does for their CAM16-based implementation.
    
    This could be more efficient in terms of calculations and garbage
    creation, but it's fast enough for now.

--- app/src/main/java/dev/kdrag0n/android12ext/monet/theme/DynamicColorScheme.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/monet/theme/DynamicColorScheme.kt
@@ -1,11 +1,15 @@
 package dev.kdrag0n.android12ext.monet.theme
 
+import dev.kdrag0n.android12ext.monet.colors.CieLab.Companion.toCieLab
+import dev.kdrag0n.android12ext.monet.colors.CieXyz.Companion.toCieXyz
 import dev.kdrag0n.android12ext.monet.colors.Color
 import dev.kdrag0n.android12ext.monet.colors.Lch
 import dev.kdrag0n.android12ext.monet.colors.Oklab.Companion.toOklab
 import dev.kdrag0n.android12ext.monet.colors.Oklch
 import dev.kdrag0n.android12ext.monet.colors.Oklch.Companion.toOklch
+import dev.kdrag0n.android12ext.monet.colors.Srgb
 import timber.log.Timber
+import kotlin.math.abs
 
 class DynamicColorScheme(
     targetColors: ColorScheme,
@@ -18,7 +22,8 @@ class DynamicColorScheme(
     private val primaryAccent = primaryNeutral
 
     init {
-        Timber.i("Primary color: ${String.format("%06x", primaryColor.toLinearSrgb().toSrgb().quantize8())} => $primaryNeutral")
+        val primaryRgb8 = primaryColor.toLinearSrgb().toSrgb().quantize8()
+        Timber.i("Primary color: ${String.format("%06x", primaryRgb8)} => $primaryNeutral")
     }
 
     // Main background color. Tinted with the primary color.
@@ -56,7 +61,8 @@ class DynamicColorScheme(
         return swatch.map { (shade, color) ->
             val target = color as? Lch
                 ?: color.toLinearSrgb().toOklab().toOklch()
-            val newLch = colorFilter(transformColor(target, primary))
+            val targetLstar = TargetColors.LSTAR_LIGHTNESS_MAP[shade]!!
+            val newLch = colorFilter(transformColor(target, primary, targetLstar))
             val newSrgb = newLch.toOklab().toLinearSrgb().toSrgb()
 
             val newRgb8 = newSrgb.quantize8()
@@ -65,20 +71,73 @@ class DynamicColorScheme(
         }.toMap()
     }
 
-    private fun transformColor(target: Lch, primary: Lch): Oklch {
-        return Oklch(
-            // Keep target luminance. Themes should never need to change it.
-            L = target.L,
-            // Allow colorless gray.
-            C = primary.C.coerceIn(0.0, target.C),
-            // Use the primary color's hue, since it's the most prominent feature of the theme.
-            h = primary.h,
-        )
+    private fun transformColor(target: Lch, primary: Lch, targetLstar: Double): Oklch {
+        // Allow colorless gray.
+        val C = primary.C.coerceIn(0.0, target.C)
+        // Use the primary color's hue, since it's the most prominent feature of the theme.
+        val h = primary.h
+        // Binary search for the target lightness
+        val L = searchLstar(targetLstar, C, h)
+
+        return Oklch(L, C, h)
+    }
+
+    private fun searchLstar(targetLstar: Double, C: Double, h: Double): Double {
+        // Some colors result in imperfect blacks (e.g. #000002) if we don't account for
+        // negative lightness.
+        var min = -0.5
+        // Colors can also be overexposed to better match CIELAB targets.
+        var max = 1.5
+
+        // Keep track of the best L value found.
+        // This will be returned if the search fails to converge.
+        var bestL = Double.NaN
+        var bestLDelta = Double.POSITIVE_INFINITY
+
+        while (true) {
+            val mid = (min + max) / 2
+
+            // The search must be done in 8-bpc sRGB to account for the effects of clipping.
+            // Otherwise, results at lightness extremes (especially ~shade 10) are quite far
+            // off after quantization and clipping.
+            val srgbClipped = Oklch(mid, C, h).toOklab().toLinearSrgb().toSrgb().quantize8()
+
+            // Convert back to Color and compare CIELAB L*
+            val lstar = Srgb(srgbClipped).toLinearSrgb().toCieXyz().toCieLab().L
+            val delta = abs(lstar - targetLstar)
+
+            if (delta < bestLDelta) {
+                bestL = mid
+                bestLDelta = delta
+            }
+
+            Timber.i("Search for L*: [target=$targetLstar] ($min, $max)=>$mid  L*=$lstar delta=$delta  (best=$bestL delta=$bestLDelta)")
+
+            when {
+                // If L* ~= target, consider the result good enough
+                delta <= TARGET_LSTAR_THRESHOLD -> return mid
+                // If min ~= max, we're unlikely to make any more progress
+                abs(min - max) <= TARGET_L_EPSILON -> return bestL
+
+                // Divide and continue
+                lstar < targetLstar -> min = mid
+                lstar > targetLstar -> max = mid
+            }
+        }
     }
 
     companion object {
         // Hue shift for the tertiary accent color (accent3), in degrees.
         // 60 degrees = shifting by a secondary color
         private const val ACCENT3_HUE_SHIFT_DEGREES = 60.0
+
+        // Threshold for matching CIELAB L* targets. Colors with lightness delta
+        // under this value are considered to match the reference lightness.
+        private const val TARGET_LSTAR_THRESHOLD = 0.01
+
+        // Threshold for terminating the binary search if min and max are too close.
+        // The search is very unlikely to make progress after this point, so we
+        // just terminate it and return the best L* value found.
+        private const val TARGET_L_EPSILON = 0.001
     }
 }
