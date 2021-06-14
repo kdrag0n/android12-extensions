e07d281e212a14682e0958890f47d12074174f9e
Danny Lin <danny@kdrag0n.dev>
2021-06-13 20:45:24 -0700

    monet: extraction: Suppress compiler warnings

--- app/src/main/java/dev/kdrag0n/android12ext/monet/extraction/WallpaperColors.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/monet/extraction/WallpaperColors.kt
@@ -3,12 +3,14 @@ package dev.kdrag0n.android12ext.monet.extraction
 import android.app.WallpaperColors
 import android.graphics.Color
 
+@Suppress("unchecked_cast")
 val WallpaperColors.mainColors: List<Color>
     get() {
         return this::class.java.getDeclaredMethod("getMainColors")
             .invoke(this) as List<Color>
     }
 
+@Suppress("unchecked_cast")
 val WallpaperColors.allColors: Map<Int, Int>
     get() {
         return this::class.java.getDeclaredMethod("getAllColors")
