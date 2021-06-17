7148509e4a1c2de764f4c805370900a21c22f90d
Danny Lin <danny@kdrag0n.dev>
2021-06-16 21:52:19 -0700

    EXPERIMENTAL: monet: theme: Switch to SRLAB2 color space
    SRLAB2 [1] is similar to Oklab in structure, based on CIELAB with parts
    from CIECAM02.
    
    [1] https://www.magnetkern.de/srlab2.html

--- app/src/main/java/dev/kdrag0n/android12ext/monet/theme/DynamicColorScheme.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/monet/theme/DynamicColorScheme.kt
@@ -4,9 +4,9 @@ import dev.kdrag0n.android12ext.monet.colors.CieLab.Companion.toCieLab
 import dev.kdrag0n.android12ext.monet.colors.CieXyz.Companion.toCieXyz
 import dev.kdrag0n.android12ext.monet.colors.Color
 import dev.kdrag0n.android12ext.monet.colors.Lch
-import dev.kdrag0n.android12ext.monet.colors.Oklab.Companion.toOklab
-import dev.kdrag0n.android12ext.monet.colors.Oklch
-import dev.kdrag0n.android12ext.monet.colors.Oklch.Companion.toOklch
+import dev.kdrag0n.android12ext.monet.colors.Srlab2.Companion.toSrlab2
+import dev.kdrag0n.android12ext.monet.colors.Srlch2
+import dev.kdrag0n.android12ext.monet.colors.Srlch2.Companion.toSrlch2
 import dev.kdrag0n.android12ext.monet.colors.Srgb
 import timber.log.Timber
 import kotlin.math.abs
@@ -17,7 +17,7 @@ class DynamicColorScheme(
     chromaMultiplier: Double = 1.0,
     private val accurateShades: Boolean = true,
 ) : ColorScheme() {
-    private val primaryNeutral = primaryColor.toLinearSrgb().toOklab().toOklch().let { lch ->
+    private val primaryNeutral = primaryColor.toLinearSrgb().toSrlab2().toSrlch2().let { lch ->
         lch.copy(C = lch.C * chromaMultiplier)
     }
     private val primaryAccent = primaryNeutral
@@ -59,10 +59,10 @@ class DynamicColorScheme(
     ): Map<Int, Color> {
         return swatch.map { (shade, color) ->
             val target = color as? Lch
-                ?: color.toLinearSrgb().toOklab().toOklch()
+                ?: color.toLinearSrgb().toSrlab2().toSrlch2()
             val targetLstar = TargetColors.LSTAR_LIGHTNESS_MAP[shade]!!
             val newLch = transformColor(target, primary, targetLstar)
-            val newSrgb = newLch.toOklab().toLinearSrgb().toSrgb()
+            val newSrgb = newLch.toSrlab2().toLinearSrgb().toSrgb()
 
             val newRgb8 = newSrgb.quantize8()
             Timber.d("Transform: [$shade] $target => $newLch => ${String.format("%06x", newRgb8)}")
@@ -70,7 +70,7 @@ class DynamicColorScheme(
         }.toMap()
     }
 
-    private fun transformColor(target: Lch, primary: Lch, targetLstar: Double): Oklch {
+    private fun transformColor(target: Lch, primary: Lch, targetLstar: Double): Srlch2 {
         // Allow colorless gray.
         val C = primary.C.coerceIn(0.0, target.C)
         // Use the primary color's hue, since it's the most prominent feature of the theme.
@@ -82,15 +82,15 @@ class DynamicColorScheme(
             target.L
         }
 
-        return Oklch(L, C, h)
+        return Srlch2(L, C, h)
     }
 
     private fun searchLstar(targetLstar: Double, C: Double, h: Double): Double {
         // Some colors result in imperfect blacks (e.g. #000002) if we don't account for
         // negative lightness.
-        var min = -0.5
+        var min = -50.0
         // Colors can also be overexposed to better match CIELAB targets.
-        var max = 1.5
+        var max = 150.0
 
         // Keep track of the best L value found.
         // This will be returned if the search fails to converge.
@@ -103,10 +103,10 @@ class DynamicColorScheme(
             // The search must be done in 8-bpc sRGB to account for the effects of clipping.
             // Otherwise, results at lightness extremes (especially ~shade 10) are quite far
             // off after quantization and clipping.
-            val srgbClipped = Oklch(mid, C, h).toOklab().toLinearSrgb().toSrgb().quantize8()
+            val srgbClipped = Srlch2(mid, C, h).toSrlab2().toLinearSrgb().toSrgb().quantize8()
 
             // Convert back to Color and compare CIELAB L*
-            val lstar = Srgb(srgbClipped).toLinearSrgb().toOklab().L
+            val lstar = Srgb(srgbClipped).toLinearSrgb().toSrlab2().L
             val delta = abs(lstar - targetLstar)
 
             if (delta < bestLDelta) {
@@ -136,7 +136,7 @@ class DynamicColorScheme(
 
         // Threshold for matching CIELAB L* targets. Colors with lightness delta
         // under this value are considered to match the reference lightness.
-        private const val TARGET_LSTAR_THRESHOLD = 0.01 / 100.0
+        private const val TARGET_LSTAR_THRESHOLD = 0.01
 
         // Threshold for terminating the binary search if min and max are too close.
         // The search is very unlikely to make progress after this point, so we
