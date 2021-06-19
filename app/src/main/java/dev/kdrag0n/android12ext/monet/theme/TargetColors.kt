fc2d2cfaa564de01cd0ad82db4904ac20af0c8fc
Danny Lin <danny@kdrag0n.dev>
2021-06-19 02:53:34 -0700

    Revert "EXPERIMENTAL: monet: theme: Switch to SRLAB2 color space"
    This reverts commit 69883ecd7a83d74d92875dfc567a4601def550ab.
    
    SRLAB2 has two apparent problems for our use case:
        - Lightness is essentially the same as CIELAB L*, which isn't as
          perceptually uniform as Oklab's lightness metric in very light
          (shade ~10) colors
        - Red swatches are too orange
    
    Example results:
        - Blue (#3f51b5): A-1 10 is too chromatic
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- app/src/main/java/dev/kdrag0n/android12ext/monet/theme/TargetColors.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/monet/theme/TargetColors.kt
@@ -1,7 +1,7 @@
 package dev.kdrag0n.android12ext.monet.theme
 
 import dev.kdrag0n.android12ext.monet.colors.Color
-import dev.kdrag0n.android12ext.monet.colors.Srlch2
+import dev.kdrag0n.android12ext.monet.colors.Oklch
 
 /*
  * Default target colors, conforming to Material You standards.
@@ -16,18 +16,18 @@ class TargetColors(
     companion object {
         // Lightness from AOSP defaults
         private val LIGHTNESS_MAP = mapOf(
-            0    to 100.00000,
-            10   to 98.61786194717564,
-            50   to 94.79598041142819,
-            100  to 89.88343723721563,
-            200  to 79.88099523671632,
-            300  to 69.98193993576817,
-            400  to 59.78854890533891,
-            500  to 49.23885747836461,
-            600  to 39.9039510598721,
-            700  to 29.724610083590353,
-            800  to 19.86549087952754,
-            900  to 9.766921621553388,
+            0    to 1.00000,
+            10   to 0.98809,
+            50   to 0.95514,
+            100  to 0.91279,
+            200  to 0.82656,
+            300  to 0.74123,
+            400  to 0.65335,
+            500  to 0.56241,
+            600  to 0.48193,
+            700  to 0.39418,
+            800  to 0.30919,
+            900  to 0.22213,
             1000 to 0.00000,
         )
 
@@ -53,7 +53,7 @@ class TargetColors(
         // A-1 chroma = avg(default Pixel Blue shades 100-900)
         // Excluding very bright variants (10, 50) to avoid light bias
         // A-1 > A-3 > A-2
-        private const val ACCENT1_CHROMA = 46.82689336772568
+        private const val ACCENT1_CHROMA = 0.13281
         private const val ACCENT2_CHROMA = ACCENT1_CHROMA / 3
         private const val ACCENT3_CHROMA = ACCENT2_CHROMA * 2
 
@@ -75,7 +75,7 @@ class TargetColors(
         val chromaAdj = chroma * chromaFactor
 
         return LIGHTNESS_MAP.map {
-            it.key to Srlch2(it.value, chromaAdj, 0.0)
+            it.key to Oklch(it.value, chromaAdj)
         }.toMap()
     }
 }
