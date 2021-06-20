44d0f732ec832173c06a7644477205a44e11cfd9
Danny Lin <danny@kdrag0n.dev>
2021-06-20 01:40:52 -0700

    Revert "EXPERIMENTAL: monet: theme: Switch to CAM16 color space"
    This reverts commit dd13f62f56e721fd697fc0e91783ca03037016da.
    
    CAM16 does a good job overall and challenges Oklab, but I think Oklab
    still wins for our use case.
    
    CAM16 pros:
        - Slightly more consistent lightness at shade 10
        - Good perceptual performance overall
    
    CAM16 cons:
        - Complex (we're currently wrapping Google's implementation)
        - Blue has a strong shift towards purple
        - Shade 10 tends to have more hue shift
        - Most low-chroma swatches (A-2, N-1, N-2) have less perceptual
          chroma than expected from the proportional chroma targets we use,
          but not all of them -- so it's inconsistent. Examples of chroma
          disparities:
            - Magenta (#9c27b0) A-2 and N-2 are similar to Oklab and other
              color spaces. N-1 is slightly different, but still close.
            - Green (#4caf50) A-2 and N-2 are also similar, while N-1 is
              slightly different.
            - Violet (#673ab7) and blue (#3f51b5) A-2, N-1, and N-2 are
              significantly less chromatic than magenta and green.
        - Some low-chroma swatches (A-2, N-1, N-2) have excessive hue shifts
          at shade 10. Examples of hue shifts:
            - Magenta (#9c27b0) A-3 #10 is yellow instead of ~red-orange
            - Violet (#673ab7) A-1, A-2, and A-3 #10 are mostly magenta in
              both CAM16 and Oklab, likely due to sRGB limits. However,
              Oklab preserves the violet hue in N-1 and N-2 #10, while CAM16
              is still magenta.
            - Blue (#3f51b5) A-3, N-1, and N-2 #10 are ~magenta due to
              CAM16's blue hue shift towards purple. However, A-1 #10 is
              blue and A-2 #10 is violet.
            - Green (#4caf50) N-1 #10 shifts towards yellow significantly.
              A-1 and A-2 #10 are also affected to a smaller extent, while
              N-2 and A-3 #10 are essentially unaffected.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- app/src/main/java/dev/kdrag0n/android12ext/monet/theme/TargetColors.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/monet/theme/TargetColors.kt
@@ -1,7 +1,7 @@
 package dev.kdrag0n.android12ext.monet.theme
 
 import dev.kdrag0n.android12ext.monet.colors.Color
-import dev.kdrag0n.android12ext.monet.colors.Cam16
+import dev.kdrag0n.android12ext.monet.colors.Oklch
 
 /*
  * Default target colors, conforming to Material You standards.
@@ -16,19 +16,19 @@ class TargetColors(
     companion object {
         // Lightness from AOSP defaults
         private val LIGHTNESS_MAP = mapOf(
-            0    to 100.0,
-            10   to 98.077286,
-            50   to 92.8308,
-            100  to 86.24347,
-            200  to 73.39204,
-            300  to 61.44706,
-            400  to 49.99679,
-            500  to 39.11102,
-            600  to 30.351782,
-            700  to 21.80448,
-            800  to 14.609172,
-            900  to 8.459497,
-            1000 to 0.0,
+            0    to 1.00000,
+            10   to 0.98809,
+            50   to 0.95514,
+            100  to 0.91279,
+            200  to 0.82656,
+            300  to 0.74123,
+            400  to 0.65335,
+            500  to 0.56241,
+            600  to 0.48193,
+            700  to 0.39418,
+            800  to 0.30919,
+            900  to 0.22213,
+            1000 to 0.00000,
         )
 
         // Accent chroma from Pixel defaults
@@ -36,7 +36,7 @@ class TargetColors(
         // A-1 chroma = avg(default Pixel Blue shades 100-900)
         // Excluding very bright variants (10, 50) to avoid light bias
         // A-1 > A-3 > A-2
-        private const val ACCENT1_CHROMA = 47.35212111111111
+        private const val ACCENT1_CHROMA = 0.13281
         private const val ACCENT2_CHROMA = ACCENT1_CHROMA / 3
         private const val ACCENT3_CHROMA = ACCENT2_CHROMA * 2
 
@@ -58,7 +58,7 @@ class TargetColors(
         val chromaAdj = chroma * chromaFactor
 
         return LIGHTNESS_MAP.map {
-            it.key to Cam16(it.value, chromaAdj, 0.0)
+            it.key to Oklch(it.value, chromaAdj)
         }.toMap()
     }
 }
