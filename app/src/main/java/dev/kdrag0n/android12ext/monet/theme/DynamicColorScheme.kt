4d27ca304e38b4b5133f8d82987d2168f31bc62a
Danny Lin <danny@kdrag0n.dev>
2021-06-21 00:03:05 -0700

    monet: theme: Switch to Oklab-based sRGB gamut clipping
    The binary search used here is really just a primitive form of gamut
    mapping. Instead of hard-clipping 8-bit sRGB values, we binary search
    and attempt to preserve the chroma. It works, but it's not ideal
    (underexposed L=-0.5 and/or overexposed L=2 result in NaN in some color
    spaces) and causes hue shifts sometimes. Additionally, some colors never
    converge, so we're left with a suboptimal color that may not match the
    target lightness closely.
    
    This numerical gamut intersection solution proposed by Björn Ottosson [1]
    gives significantly better results than our binary search while also
    running much faster. I haven't noticed any significant hue shifts with
    it, whereas the old solution often struggled with shade 10 and sometimes
    50. Perceptual lightness is more consistent as well, since there won't
    be issues with some searches not converging.
    
    The new solution also helps accommodate people who prefer maximum chroma
    over accurate shades. Instead of hard-clipping, another clipping method
    can be used to generate similar colors while avoiding hue shifts:
        - Default clipping method: preserve lightness (L0 = target L)
        - Inaccurate clipping method: project to L0 = L_cusp
    
    [1] https://bottosson.github.io/posts/gamutclipping/

--- app/src/main/java/dev/kdrag0n/android12ext/monet/theme/DynamicColorScheme.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/monet/theme/DynamicColorScheme.kt
@@ -1,13 +1,9 @@
 package dev.kdrag0n.android12ext.monet.theme
 
-import dev.kdrag0n.android12ext.monet.colors.Color
-import dev.kdrag0n.android12ext.monet.colors.Lch
+import dev.kdrag0n.android12ext.monet.colors.*
 import dev.kdrag0n.android12ext.monet.colors.Oklab.Companion.toOklab
-import dev.kdrag0n.android12ext.monet.colors.Oklch
 import dev.kdrag0n.android12ext.monet.colors.Oklch.Companion.toOklch
-import dev.kdrag0n.android12ext.monet.colors.Srgb
 import timber.log.Timber
-import kotlin.math.abs
 
 class DynamicColorScheme(
     targetColors: ColorScheme,
@@ -59,7 +55,7 @@ class DynamicColorScheme(
             val target = color as? Lch
                 ?: color.toLinearSrgb().toOklab().toOklch()
             val newLch = transformColor(target, primary)
-            val newSrgb = newLch.toOklab().toLinearSrgb().toSrgb()
+            val newSrgb = newLch.toLinearSrgb().toSrgb()
 
             val newRgb8 = newSrgb.quantize8()
             Timber.d("Transform: [$shade] $target => $newLch => ${String.format("%06x", newRgb8)}")
@@ -67,77 +63,29 @@ class DynamicColorScheme(
         }.toMap()
     }
 
-    private fun transformColor(target: Lch, primary: Lch): Oklch {
+    private fun transformColor(target: Lch, primary: Lch): Color {
+        // Keep target lightness.
+        val L = target.L
         // Allow colorless gray.
         val C = primary.C.coerceIn(0.0, target.C)
         // Use the primary color's hue, since it's the most prominent feature of the theme.
         val h = primary.h
-        // Binary search for the target lightness for accuracy
-        val L = if (accurateShades) {
-            findSrgbLightness(target.L, C, h)
+
+        val srgb = Oklch(L, C, h).toLinearSrgb()
+        val clipMethod = if (accurateShades) {
+            // Prefer lightness
+            OklabGamut.ClipMethod.PRESERVE_LIGHTNESS
         } else {
-            target.L
+            // Prefer chroma
+            OklabGamut.ClipMethod.PROJECT_TO_LCUSP
         }
 
-        return Oklch(L, C, h)
-    }
-
-    private fun findSrgbLightness(targetL: Double, C: Double, h: Double): Double {
-        // Some colors result in imperfect blacks (e.g. #000002) if we don't account for
-        // negative lightness.
-        var min = -0.5
-        // Colors can also be overexposed to better match CIELAB targets.
-        var max = 1.5
-
-        // Keep track of the best L value found.
-        // This will be returned if the search fails to converge.
-        var bestL = Double.NaN
-        var bestLDelta = Double.POSITIVE_INFINITY
-
-        while (true) {
-            val mid = (min + max) / 2
-
-            // The search must be done in 8-bpc sRGB to account for the effects of clipping.
-            // Otherwise, results at lightness extremes (especially ~shade 10) are quite far
-            // off after quantization and clipping.
-            val srgbClipped = Oklch(mid, C, h).toOklab().toLinearSrgb().toSrgb().quantize8()
-
-            // Convert back to Oklab and compare lightness
-            val l = Srgb(srgbClipped).toLinearSrgb().toOklab().L
-            val delta = abs(l - targetL)
-
-            if (delta < bestLDelta) {
-                bestL = mid
-                bestLDelta = delta
-            }
-
-            Timber.i("Search for L: [target=$targetL] ($min, $max)=>$mid  L=$l delta=$delta  (best=$bestL delta=$bestLDelta)")
-
-            when {
-                // If L* ~= target, consider the result good enough
-                delta <= TARGET_L_DELTA -> return mid
-                // If min ~= max, we're unlikely to make any more progress
-                abs(min - max) <= TARGET_L_EPSILON -> return bestL
-
-                // Divide and continue
-                l < targetL -> min = mid
-                l > targetL -> max = mid
-            }
-        }
+        return OklabGamut.clip(srgb, clipMethod)
     }
 
     companion object {
         // Hue shift for the tertiary accent color (accent3), in degrees.
         // 60 degrees = shifting by a secondary color
         private const val ACCENT3_HUE_SHIFT_DEGREES = 60.0
-
-        // Threshold for matching lightness targets. Colors with lightness delta
-        // under this value are considered to match the reference lightness.
-        private const val TARGET_L_DELTA = 0.01 / 100.0
-
-        // Threshold for terminating the binary search if min and max are too close.
-        // The search is very unlikely to make progress after this point, so we
-        // just terminate it and return the best L* value found.
-        private const val TARGET_L_EPSILON = 0.001
     }
 }
