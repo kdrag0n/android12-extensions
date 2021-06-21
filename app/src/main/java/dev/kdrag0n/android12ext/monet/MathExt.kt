8b8164f6c112bbd1dcee3185173bcb7384c279c3
Danny Lin <danny@kdrag0n.dev>
2021-06-20 23:26:10 -0700

    monet: colors: Optimize Oklab gamut clipping and improve API

--- app/src/main/java/dev/kdrag0n/android12ext/monet/MathExt.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/monet/MathExt.kt
@@ -1,3 +1,7 @@
+// These simple math functions should always be inlined for performance
+@file:Suppress("NOTHING_TO_INLINE")
+
 package dev.kdrag0n.android12ext.monet
 
-internal fun cube(x: Double) = x * x * x
+internal inline fun cube(x: Double) = x * x * x
+internal inline fun square(x: Double) = x * x
