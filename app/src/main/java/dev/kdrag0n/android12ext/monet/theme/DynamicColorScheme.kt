fe31b13e6928d558364b0004a6a161289ccf47a3
Danny Lin <danny@kdrag0n.dev>
2021-06-22 15:42:21 -0700

    monet: theme: Preserve chroma ratios when clamping chroma
    We clamp chroma instead of setting it to a constant so that low-chroma
    colors like brown don't become orange/red. However, clamping chroma on a
    per-swatch basis breaks chroma ratios -- e.g. A-1 becomes the same as
    A-2.
    
    Scale chroma relative to A-1 / N-1 (depending on the swatch) to fix the
    issue while still allowing input colors to express chroma.

--- app/src/main/java/dev/kdrag0n/android12ext/monet/theme/DynamicColorScheme.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/monet/theme/DynamicColorScheme.kt
@@ -21,40 +21,43 @@ class DynamicColorScheme(
         Timber.i("Seed color: ${String.format("%06x", seedRgb8)} => $seedNeutral")
     }
 
-    // Main background color. Tinted with the seed color.
-    override val neutral1 by lazy(mode = LazyThreadSafetyMode.NONE) {
-        transformSwatch(targets.neutral1, seedNeutral)
-    }
-
-    // Secondary background color. Slightly tinted with the seed color.
-    override val neutral2 by lazy(mode = LazyThreadSafetyMode.NONE) {
-        transformSwatch(targets.neutral2, seedNeutral)
-    }
-
     // Main accent color. Generally, this is close to the seed color.
     override val accent1 by lazy(mode = LazyThreadSafetyMode.NONE) {
-        transformSwatch(targets.accent1, seedAccent)
+        transformSwatch(targets.accent1, seedAccent, targets.accent1)
     }
 
     // Secondary accent color. Darker shades of accent1.
     override val accent2 by lazy(mode = LazyThreadSafetyMode.NONE) {
-        transformSwatch(targets.accent2, seedAccent)
+        transformSwatch(targets.accent2, seedAccent, targets.accent1)
     }
 
     // Tertiary accent color. Seed color shifted to the next secondary color via hue offset.
     override val accent3 by lazy(mode = LazyThreadSafetyMode.NONE) {
         val seedA3 = seedAccent.copy(h = seedAccent.h + ACCENT3_HUE_SHIFT_DEGREES)
-        transformSwatch(targets.accent3, seedA3)
+        transformSwatch(targets.accent3, seedA3, targets.accent1)
+    }
+
+    // Main background color. Tinted with the seed color.
+    override val neutral1 by lazy(mode = LazyThreadSafetyMode.NONE) {
+        transformSwatch(targets.neutral1, seedNeutral, targets.neutral1)
+    }
+
+    // Secondary background color. Slightly tinted with the seed color.
+    override val neutral2 by lazy(mode = LazyThreadSafetyMode.NONE) {
+        transformSwatch(targets.neutral2, seedNeutral, targets.neutral1)
     }
 
     private fun transformSwatch(
         swatch: ColorSwatch,
         seed: Lch,
+        referenceSwatch: ColorSwatch,
     ): ColorSwatch {
         return swatch.map { (shade, color) ->
             val target = color as? Lch
                 ?: color.toLinearSrgb().toOklab().toOklch()
-            val newLch = transformColor(target, seed)
+            val reference = referenceSwatch[shade]!! as? Lch
+                ?: color.toLinearSrgb().toOklab().toOklch()
+            val newLch = transformColor(target, seed, reference)
             val newSrgb = newLch.toLinearSrgb().toSrgb()
 
             val newRgb8 = newSrgb.quantize8()
@@ -63,11 +66,12 @@ class DynamicColorScheme(
         }.toMap()
     }
 
-    private fun transformColor(target: Lch, seed: Lch): Color {
+    private fun transformColor(target: Lch, seed: Lch, reference: Lch): Color {
         // Keep target lightness.
         val L = target.L
-        // Allow colorless gray.
-        val C = seed.C.coerceIn(0.0, target.C)
+        // Allow colorless gray and low-chroma colors by clamping.
+        // To preserve chroma ratios, scale chroma by the reference (A-1 / N-1).
+        val C = target.C * (seed.C.coerceIn(0.0, reference.C) / reference.C)
         // Use the seed color's hue, since it's the most prominent feature of the theme.
         val h = seed.h
 
