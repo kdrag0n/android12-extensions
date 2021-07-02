0208c08f960bdc079c77b86b32137745b06a57fa
Danny Lin <danny@kdrag0n.dev>
2021-07-02 03:44:42 -0700

    build: Update AndroidX core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
 
-    implementation("androidx.core:core-ktx:1.5.0")
+    implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("androidx.fragment:fragment:1.3.5")
     implementation("com.google.android.material:material:1.4.0-rc01")
