510cfa884b01428dec1b5655713c3d99a6f3aaf6
Danny Lin <danny@kdrag0n.dev>
2021-07-16 03:41:49 -0700

    customize: Update for Beta 3

--- magisk/customize.sh
+++ magisk/customize.sh
@@ -1,8 +1,8 @@
 #!/system/bin/sh
 
 beta_ver="$(getprop ro.build.id | cut -d. -f1 | sed 's/SPB//')"
-if ! getprop ro.build.id | grep -q SPB || [[ "$beta_ver" -ne 2 ]]; then
-    ui_print "This module is ONLY for Android 12 Beta 2."
+if ! getprop ro.build.id | grep -q SPB || [[ "$beta_ver" -ne 3 ]]; then
+    ui_print "This module is ONLY for Android 12 Beta 3."
 
     # Abort install and clean up
     rm -fr $TMPDIR $MODPATH
