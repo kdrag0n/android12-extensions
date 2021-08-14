457a967d886f221edd0ded481c69b2c72281d687
Danny Lin <danny@kdrag0n.dev>
2021-08-14 04:25:49 -0700

    monet: Import MathExt from ZCAM branch

--- app/src/main/java/dev/kdrag0n/android12ext/monet/util/MathExt.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/monet/util/MathExt.kt
@@ -1,8 +1,13 @@
 // These simple math functions should always be inlined for performance
 @file:Suppress("NOTHING_TO_INLINE")
 
-package dev.kdrag0n.android12ext.monet
+package dev.kdrag0n.android12ext.monet.util
+
+import kotlin.math.PI
 
 internal inline fun cube(x: Double) = x * x * x
 internal inline fun square(x: Double) = x * x
 internal inline fun square(x: Float) = x * x
+
+internal fun Double.toRadians() = this * PI / 180.0
+internal fun Double.toDegrees() = this * 180.0 / PI
