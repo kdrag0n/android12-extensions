da356c39bf3b5a280ac45ad193ba066b5a32f8a9
Danny Lin <danny@kdrag0n.dev>
2021-08-15 00:40:30 -0700

    build: Update Gradle plugins and drop R8 hotfix

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,17 +6,12 @@ buildscript {
         mavenCentral()
         google()
         maven("https://plugins.gradle.org/m2/")
-        maven("https://storage.googleapis.com/r8-releases/raw")
     }
 
     dependencies {
-        // Fix https://issuetracker.google.com/issues/192023718
-        // TODO: Remove once we update to AGP 7.0
-        classpath("com.android.tools:r8:2.2.77")
-
         classpath("com.android.tools.build:gradle:7.0.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("com.mikepenz.aboutlibraries.plugin:aboutlibraries-plugin:8.9.0")
+        classpath("com.mikepenz.aboutlibraries.plugin:aboutlibraries-plugin:8.9.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.37")
 
         // NOTE: Do not place your application dependencies here; they belong
