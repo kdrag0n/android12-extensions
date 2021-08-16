6616ed040341801138c512411c9b5d36a5595667
Danny Lin <danny@kdrag0n.dev>
2021-08-15 18:07:44 -0700

    data: settings: Make white luminance functions private

--- app/src/main/java/dev/kdrag0n/android12ext/data/SettingsRepository.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/data/SettingsRepository.kt
@@ -23,19 +23,19 @@ class SettingsRepository @Inject constructor(
 
     companion object {
         private const val WHITE_LUMINANCE_MIN = 1.0
-        const val WHITE_LUMINANCE_MAX = 10000.0
+        private const val WHITE_LUMINANCE_MAX = 10000.0
         const val WHITE_LUMINANCE_USER_MAX = 1000
         const val WHITE_LUMINANCE_USER_STEP = 25 // both max and default must be divisible by this
         const val WHITE_LUMINANCE_USER_DEFAULT = 425 // ~200.0 divisible by step (decoded = 199.526)
 
-        fun parseWhiteLuminanceUser(userValue: Int): Double {
+        private fun parseWhiteLuminanceUser(userValue: Int): Double {
             val userSrc = userValue.toDouble() / WHITE_LUMINANCE_USER_MAX
             val userInv = 1.0 - userSrc
             return (10.0).pow(userInv * log10(WHITE_LUMINANCE_MAX))
                 .coerceAtLeast(WHITE_LUMINANCE_MIN)
         }
 
-        fun encodeWhiteLuminanceUser(luminance: Double): Int {
+        private fun encodeWhiteLuminanceUser(luminance: Double): Int {
             val userInv = log10(luminance.coerceAtLeast(WHITE_LUMINANCE_MIN)) /
                     log10(WHITE_LUMINANCE_MAX)
             return ((1.0 - userInv) * WHITE_LUMINANCE_USER_MAX).roundToInt()
