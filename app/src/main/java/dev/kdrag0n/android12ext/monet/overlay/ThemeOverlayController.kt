4d6c7f8f1411e0970fd72a799d06118b0e164b09
Danny Lin <danny@kdrag0n.dev>
2021-08-18 02:24:47 -0700

    monet: overlay: Overlay modulated surface ColorStateLists
    To improve quality, we can actually overlay the ColorStateList XMLs and
    change them to color types instead of modulating them with Cam at
    runtime.

--- app/src/main/java/dev/kdrag0n/android12ext/monet/overlay/ThemeOverlayController.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/monet/overlay/ThemeOverlayController.kt
@@ -2,7 +2,8 @@ package dev.kdrag0n.android12ext.monet.overlay
 
 import android.app.WallpaperColors
 import android.content.Context
-import android.graphics.Color
+import androidx.annotation.ColorInt
+import dev.kdrag0n.android12ext.monet.colors.Color
 import dev.kdrag0n.android12ext.monet.colors.Srgb
 import dev.kdrag0n.android12ext.xposed.hooks.ColorSchemeFactory
 import timber.log.Timber
@@ -15,39 +16,53 @@ class ThemeOverlayController(
     fun getAccentColor(colors: WallpaperColors) = getNeutralColor(colors)
 
     // com.android.systemui.theme.ThemeOverlayController#getOverlay(int, int)
-    fun getOverlay(primaryColor: Int, isAccent: Int): Any {
+    fun getOverlay(primaryColor: Int, type: Int): Any {
         // Generate color scheme
         val colorScheme = colorSchemeFactory.getColor(Srgb(primaryColor))
 
-        val groupKey = when (isAccent) {
-            1 -> "accent"
-            else -> "neutral"
+        val (groupKey, colorsList) = when (type) {
+            TYPE_NEUTRAL -> "neutral" to colorScheme.neutralColors
+            TYPE_ACCENT -> "accent" to colorScheme.accentColors
+            else -> error("Unknown type $type")
         }
 
         return FabricatedOverlay.Builder("com.android.systemui", groupKey, "android").run {
-            val colorsList = when (isAccent) {
-                1 -> colorScheme.accentColors
-                else -> colorScheme.neutralColors
-            }
-
             colorsList.withIndex().forEach { listEntry ->
                 val group = "$groupKey${listEntry.index + 1}"
 
                 listEntry.value.forEach { (shade, color) ->
                     val colorSrgb = color as? Srgb
                         ?: color.toLinearSrgb().toSrgb()
-                    val colorRgb8 = colorSrgb.quantize8()
-                    Timber.d("Color $group $shade = ${String.format("%06x", colorRgb8)}")
+                    Timber.d("Color $group $shade = ${String.format("%06x", colorSrgb.quantize8())}")
 
-                    // Set alpha to 255
-                    val argbColor = Color.argb(255, 0, 0, 0) or colorRgb8
-
-                    val resKey = "android:color/system_${group}_$shade"
-                    setResourceValue(resKey, FabricatedOverlay.DATA_TYPE_COLOR, argbColor)
+                    setColor("system_${group}_$shade", colorSrgb)
                 }
             }
 
+            // Override special modulated surface colors
+            if (type == TYPE_NEUTRAL) {
+                // surface light = neutral1 20 (L* 98)
+                colorsList[0][20]?.let { setColor("surface_light", it) }
+
+                // surface highlight dark = neutral1 650 (L* 35)
+                colorsList[0][650]?.let { setColor("surface_highlight_dark", it) }
+            }
+
             build()
+        }
+    }
+
+    companion object {
+        private const val TYPE_NEUTRAL = 0
+        private const val TYPE_ACCENT = 1
+
+        private fun FabricatedOverlay.Builder.setColor(name: String, @ColorInt color: Int) =
+            setResourceValue("android:color/$name", FabricatedOverlay.DATA_TYPE_COLOR, color)
+
+        private fun FabricatedOverlay.Builder.setColor(name: String, color: Color): FabricatedOverlay.Builder {
+            val rgb = color.toLinearSrgb().toSrgb().quantize8()
+            val argb = rgb or (0xff shl 24)
+            return setColor(name, argb)
         }
     }
 }
