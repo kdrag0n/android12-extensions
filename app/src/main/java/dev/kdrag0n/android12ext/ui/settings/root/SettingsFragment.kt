7fc206f81e469e8d04ae2c6dc6aead897756cc6c
Danny Lin <danny@kdrag0n.dev>
2021-08-27 18:14:38 -0700

    ui: settings: Add menu item to force report settings

--- app/src/main/java/dev/kdrag0n/android12ext/ui/settings/root/SettingsFragment.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/ui/settings/root/SettingsFragment.kt
@@ -5,16 +5,22 @@ import android.view.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 import dev.kdrag0n.android12ext.R
+import dev.kdrag0n.android12ext.data.SettingsRepository
 import dev.kdrag0n.android12ext.ui.observeNav
 import dev.kdrag0n.android12ext.ui.settings.BaseSettingsFragment
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SettingsFragment : BaseSettingsFragment(), Toolbar.OnMenuItemClickListener {
     private val viewModel: SettingsViewModel by viewModels()
+
+    @Inject lateinit var settingsRepo: SettingsRepository
 
     private var telemetryDialog: AlertDialog? = null
 
@@ -55,6 +61,12 @@ class SettingsFragment : BaseSettingsFragment(), Toolbar.OnMenuItemClickListener
         }
         R.id.action_about -> {
             findNavController().navigate(R.id.action_settings_root_to_about)
+            true
+        }
+        R.id.action_report_settings -> {
+            lifecycleScope.launch {
+                settingsRepo.reportSettings()
+            }
             true
         }
         else -> false
