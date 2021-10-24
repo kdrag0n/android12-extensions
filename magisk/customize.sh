460e8bef6c041703b567f361208b93e9e214a621
Danny Lin <danny@kdrag0n.dev>
2021-10-24 04:13:53 -0700

    magisk: Allow installation on stable Android 12

--- magisk/customize.sh
+++ magisk/customize.sh
@@ -1,8 +1,7 @@
 #!/system/bin/sh
 
-beta_ver="$(getprop ro.build.id | cut -d. -f1 | sed 's/SPB//')"
-if ! getprop ro.build.id | grep -q SPB || [[ "$beta_ver" -ne 5 ]]; then
-    ui_print "This module is ONLY for Android 12 Beta 5."
+if [[ "$(getprop ro.build.id)" != "SP1A."* ]]; then
+    ui_print "This module is ONLY for Android 12 v1."
 
     # Abort install and clean up
     rm -fr $TMPDIR $MODPATH
