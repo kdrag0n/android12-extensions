17197ef60a786d9550185c69df6e34055d7ea94b
Danny Lin <danny@kdrag0n.dev>
2021-11-08 12:26:49 -0800

    hooks: ColorScheme: Fail gracefully on AOSP

--- app/src/main/java/dev/kdrag0n/android12ext/xposed/hooks/ColorSchemeHooks.kt
+++ app/src/main/java/dev/kdrag0n/android12ext/xposed/hooks/ColorSchemeHooks.kt
@@ -1,5 +1,6 @@
 package dev.kdrag0n.android12ext.xposed.hooks
 
+import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 import dev.kdrag0n.android12ext.monet.theme.ColorSchemeFactory
 import dev.kdrag0n.colorkt.conversion.ConversionGraph.convert
@@ -16,24 +17,28 @@ class ColorSchemeHooks(
             lastCamColor.set(args[0] as Int)
         }
 
-        hookReplace("com.google.material.monet.Shades", shadesOfName) {
-            val chroma = args[1] as Float
-            val theme = colorSchemeFactory.getColor(Srgb(lastCamColor.get()))
+        try {
+            hookReplace("com.google.material.monet.Shades", shadesOfName) {
+                val chroma = args[1] as Float
+                val theme = colorSchemeFactory.getColor(Srgb(lastCamColor.get()))
 
-            val swatch = when {
-                chroma >= 48.0f -> theme.accent1
-                chroma == 16.0f -> theme.accent2
-                chroma == 32.0f -> theme.accent3
-                chroma == 4.0f -> theme.neutral1
-                chroma == 8.0f -> theme.neutral2
-                else -> error("Unknown chroma $chroma")
+                val swatch = when {
+                    chroma >= 48.0f -> theme.accent1
+                    chroma == 16.0f -> theme.accent2
+                    chroma == 32.0f -> theme.accent3
+                    chroma == 4.0f -> theme.neutral1
+                    chroma == 8.0f -> theme.neutral2
+                    else -> error("Unknown chroma $chroma")
+                }
+
+                swatch.entries
+                    .filter { it.key in GOOGLE_SHADES }
+                    .sortedBy { it.key }
+                    .map { it.value.convert<Srgb>().toRgb8() or (0xff shl 24) }
+                    .toIntArray()
             }
-
-            swatch.entries
-                .filter { it.key in GOOGLE_SHADES }
-                .sortedBy { it.key }
-                .map { it.value.convert<Srgb>().toRgb8() or (0xff shl 24) }
-                .toIntArray()
+        } catch (e: XposedHelpers.ClassNotFoundError) {
+            // Ignore: only present on Pixel stock
         }
     }
 
